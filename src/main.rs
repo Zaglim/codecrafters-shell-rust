384aab1578a1a5dcec86447f2d2a8e08ce384e5e
Zaglim <lezaglim@gmail.com>
2025-01-30 14:29:43 +1100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use once_cell::unsync::Lazy;
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::path::Path;
 
+const PATH: Lazy<String> = Lazy::new(|| std::env::var("PATH").unwrap());
 fn main() {
+    // PATH.set(std::env::var("PATH").unwrap()).unwrap();
+
     let stdin = io::stdin();
 
     loop {
@@ -17,33 +22,49 @@ fn main() {
         let (command_str, following) = input.split_once(' ').unwrap_or((input, ""));
 
         match command_str.try_into() {
-            Err(_) => println!("{}: command not found", command_str),
             Ok(command) => {
-                if matches!(command, Command::Exit) {
+                if matches!(command, BuiltinCommand::Exit) {
                     break;
                 } else {
                     command.run_with(following);
                 }
             }
+            Err(_) => {
+                println!("{}: command not found", command_str)
+            }
         }
     }
 }
 
-enum Command {
+fn first_match_in_path<'a>(name: &str) -> Option<Box<Path>> {
+    for path_str in PATH.split(':') {
+        let path = Path::new(path_str).join(name);
+        if path.is_file() {
+            return Some(Box::from(path));
+        }
+    }
+    None
+}
+
+enum BuiltinCommand {
     Echo,
     Type,
     Exit,
 }
 
-impl Command {
+impl BuiltinCommand {
     fn run_with(&self, args_str: &str) {
         match self {
-            Command::Echo => println!("{args_str}"),
-            Command::Type => {
-                if let Ok(_) = Command::try_from(args_str) {
+            BuiltinCommand::Echo => println!("{args_str}"),
+            BuiltinCommand::Type => {
+                if let Ok(_) = BuiltinCommand::try_from(args_str) {
                     println!("{args_str} is a shell builtin");
                 } else {
-                    println!("{args_str}: not found")
+                    if let Some(path) = first_match_in_path(args_str) {
+                        println!("{args_str} is {}", path.display());
+                    } else {
+                        println!("{args_str}: not found")
+                    }
                 }
             }
             _ => unimplemented!(),
@@ -51,11 +72,11 @@ impl Command {
     }
 }
 
-impl TryFrom<&str> for Command {
+impl TryFrom<&str> for BuiltinCommand {
     type Error = ();
 
-    fn try_from(value: &str) -> Result<Command, Self::Error> {
-        use Command::*;
+    fn try_from(value: &str) -> Result<BuiltinCommand, Self::Error> {
+        use BuiltinCommand::*;
         match value {
             "echo" => Ok(Echo),
             "type" => Ok(Type),
